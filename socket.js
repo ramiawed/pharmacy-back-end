8da20a1124591be5ded887e196e2b59552079ceb
ramiawed <48353547+ramiawed@users.noreply.github.com>
2021-10-26 20:31:05 +0300

    update 26-10-2021

--- socket.js
+++ socket.js
@@ -8,7 +8,7 @@ const userStream = User.watch();
 // const itemStream = Item.watch();
 
 userStream.on("change", (change) => {
-  console.log(change); // You could parse out the needed info and send only that data.
+  // console.log(change); // You could parse out the needed info and send only that data.
   io.emit("changeData", change);
 });
 
