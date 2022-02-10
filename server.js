c1f7d4973d277c602ada16dd1ef21246418ebb3c
ramiawed <48353547+ramiawed@users.noreply.github.com>
2022-02-10 02:18:31 +0200

    update 9-2-2022

--- server.js
+++ server.js
@@ -32,15 +32,31 @@ let io = require("socket.io")(httpServer, {
 const User = require("./models/userModel");
 const Order = require("./models/orderModel");
 const Notification = require("./models/notificationModel");
+const Setting = require("./models/settingModel");
 
 const userStream = User.watch();
 const orderStream = Order.watch();
 const notificationStream = Notification.watch();
+const settingStream = Setting.watch();
 // const itemStream = Item.watch();
 
 userStream.on("change", (change) => {
-  // console.log(change); // You could parse out the needed info and send only that data.
-  io.emit("changeData", change);
+  console.log(change); // You could parse out the needed info and send only that data.
+  if (change.operationType === "update") {
+    if (
+      (Object.keys(change.updateDescription.updatedFields).includes(
+        "isApproved"
+      ) &&
+        change.updateDescription.updatedFields.isApproved === false) ||
+      (Object.keys(change.updateDescription.updatedFields).includes(
+        "isActive"
+      ) &&
+        change.updateDescription.updatedFields.isActive === false)
+    ) {
+      io.emit("user-sign-out", change.documentKey._id);
+    }
+  }
+  io.emit("user-changed", change);
 });
 
 orderStream.on("change", (change) => {
@@ -51,6 +67,11 @@ orderStream.on("change", (change) => {
 notificationStream.on("change", (change) => {
   console.log(change);
   io.emit("notification-changed", change);
+});
+
+settingStream.on("change", (change) => {
+  console.log(change);
+  io.emit("settings-changed", change);
 });
 
 // DATABASE CONFIGURATION OPTION {USERNAME, PASSWORD, DBNAME}
